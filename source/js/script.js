1c36bbcc164f282d3bfd61f73b9962f8959f7151
Hai Ha Nguyen <haicorp87@gmail.com>
2018-02-17 19:05:58 +0100

    cleaning code

--- source/js/script.js
+++ source/js/script.js
@@ -1,30 +1,5 @@
 // Create a "close" button and append it to each list item
 
-/* function createCloseBtn () {
-  var myNodelist = document.getElementsByTagName("LI");
-  var i;
-
-  for (i = 0; i < myNodelist.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
-  }
-} */
-
-/* function createEditBtn () {
-  var myNodelist = document.getElementsByTagName("LI");
-  var i;
-
-  for (i = 0; i < myNodelist.length; i++) {
-    var editBtn = document.createElement("button");
-    var txt = document.createTextNode("edit");
-    editBtn.className = "edit-btn";
-    editBtn.appendChild(txt);
-    myNodelist[i].appendChild(editBtn);
-  }
-} */
 var myNodelist = document.getElementsByTagName("li");
 var close = document.getElementsByClassName("checkbox-btn");
 var addBtn = document.getElementById('addBtn');
@@ -58,13 +33,6 @@ for (var i = 0; i < close.length; i++) {
   });
 }
 
-// Add a "checked" symbol when clicking on a list item
-/* var list = document.querySelector('ul');
-list.addEventListener('click', function (ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false); */
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
